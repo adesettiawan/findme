28b2299a275c3f64dfec5d627ec47ad84c37e4a1
Rheya <108859605+Rheynanta@users.noreply.github.com>
2022-07-16 00:22:21 +0700

    beberapa aja, gk bnyk

--- application/views/templates/auth_header.php
+++ application/views/templates/auth_header.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
+    <link rel="icon" href="<?= base_url('assets/img/profile/search.png'); ?>" type="image/gif" sizes="16x16">
     <title><?= $title; ?></title>
 
     <!-- Custom fonts for this template-->
